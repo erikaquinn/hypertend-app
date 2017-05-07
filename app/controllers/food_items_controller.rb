49ab29c504d3ce9e187ad142fe3494a7bfaddc7f
Erika Quinn <quinnerika16@gmail.com>
2017-05-07 16:41:30 -0500

    create index and action routes for blood_pressure controller

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -19,16 +19,34 @@ class FoodItemsController < ApplicationController
       potassium: params[:potassium],
       sugars: params[:sugars],
       notes: params[:notes],
-      meal_id: params[:meal_id]
+      meal_id: params[:meal_id],
+      user_id: current_user.id
     )
     food_item.save
     flash[:success] = "Item sucessfully added!"
-    redirect_to "create.html.erb"
+    redirect_to "/food_items"
   end
 
   def edit
     food_item_id = params[:id]
     @food_item = FoodItem.find_by(id: food_item_id)
     render "edit.html.erb"
+  end
+
+  def update
+    food_item_id = params[:id]
+    @food_item = FoodItem.find_by(id: food_item_id)
+    @description = params[:description]
+    @sodium = params[:sodium]
+    @calories = params[:calories]
+    @cholesterol = params[:cholesterol]
+    @potassium = params[:potassium]
+    @sugars = params[:sugars]
+    @notes = params[:notes]
+    @meal_id = params[:meal_id]
+    
+    food_item.save
+    flash[:success] = "Item sucessfully updated!"
+    redirect_to "/food_items/#{@food_item.id}"
   end
 end
